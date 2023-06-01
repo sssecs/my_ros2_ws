bc020746485dae0f6e42910ee787c1ec61069617
sssecs <ssssssecs@gmail.com>
2023-06-01 22:40:01 +0800

    add p3d absluate tracker

--- install/toybox1_bringup/share/toybox1_bringup/local_setup.sh
+++ install/toybox1_bringup/share/toybox1_bringup/local_setup.sh
@@ -2,7 +2,7 @@
 
 # since this file is sourced use either the provided AMENT_CURRENT_PREFIX
 # or fall back to the destination set at configure time
-: ${AMENT_CURRENT_PREFIX:="/home/sykes/ros2_ws/install/toybox1_bringup"}
+: ${AMENT_CURRENT_PREFIX:="/home/sykes/my_ros2_ws/install/toybox1_bringup"}
 if [ ! -d "$AMENT_CURRENT_PREFIX" ]; then
   if [ -z "$COLCON_CURRENT_PREFIX" ]; then
     echo "The compile time prefix path '$AMENT_CURRENT_PREFIX' doesn't " \
